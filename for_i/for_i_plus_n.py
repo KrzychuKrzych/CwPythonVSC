7b2f1a0f02f0baae69d09834244972672ebb547e
Krzysztof <kszyneg@gmail.com>
2024-05-20 18:16:18 +0200

    exercices with for and zip

---
+++ for_i/for_i_plus_n.py
@@ -0,0 +1,8 @@
+n = int(input('podaj liczbe: '))
+
+first = 2
+another = 3
+
+for i in range(n):
+    seq = first + i * another
+    print(seq)
---
+++ for_i/for_zip.py
@@ -0,0 +1,5 @@
+students = ['Anna','Maria','Damian']
+pkt = [23,44,12]
+
+for i,j in zip(students, pkt):
+    print((i,j))
